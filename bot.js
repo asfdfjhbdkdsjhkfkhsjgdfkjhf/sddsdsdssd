d35dea93158d5a0e053083ab76a9acd0ae22e868
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-11-24 20:06:19 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('guildMemberAdd', member => {
-const falsteni= member.guild.channels.get("514808324571529346");
-if(!falsteni) return;
-if(falsteni) {
-setTimeout(() => falsteni.send(`**Welcome To __ # - Versé Server :rose: :heart: __ **`), 4000)        
-}
-});
+client.on("guildMemberAdd", member => {
+        if(member.guild.id === "514339334644695060") { 
+setTimeout(function(){
+  const channel = member.guild.channels.find('id', '514808324571529346'); 
+if (!channel) return;
+  channel.send(`**Welcome To __ # - Versé Server :rose: :heart: __ ** `) 
+}, 5000); 
+}});
    
 client.login(process.env.BOT_TOKEN);
